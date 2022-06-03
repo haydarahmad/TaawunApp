0e5fbe3c31220c7dda39d9626f22b718cbfe5b0d
haydarahmad <fattanhay0@gmail.com>
2022-06-03 15:00:38 +0700

    update row item

--- app/src/main/java/com/haydar/taawun_app/MainActivity.kt
+++ app/src/main/java/com/haydar/taawun_app/MainActivity.kt
@@ -3,7 +3,7 @@ package com.haydar.taawun_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class zMainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
