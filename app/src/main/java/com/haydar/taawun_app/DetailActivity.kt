b46e02d82e4f9d0365c1ba59401657b866233bd1
haydarahmad <fattanhay0@gmail.com>
2022-07-06 22:15:29 +0700

    api untuk nampilkan daftar doa

--- app/src/main/java/com/haydar/taawun_app/DetailActivity.kt
+++ app/src/main/java/com/haydar/taawun_app/DetailActivity.kt
@@ -2,7 +2,10 @@ package com.haydar.taawun_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class DetailActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
--- app/src/main/java/com/haydar/taawun_app/ui/detail/DoaAdapter.kt
+++ app/src/main/java/com/haydar/taawun_app/ui/detail/DoaAdapter.kt
@@ -1,8 +1,10 @@
 package com.haydar.taawun_app.ui.detail
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.haydar.taawun_app.DetailActivity
 import com.haydar.taawun_app.data.ResponseDoa
 import com.haydar.taawun_app.databinding.RowItemDaftarBinding
 
@@ -24,6 +26,12 @@ class DoaAdapter : RecyclerView.Adapter<DoaAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.binding.apply {
             tvNamaDoa.text = listDoa[position].doa
+        }
+        holder.itemView.setOnClickListener{
+            holder.itemView.context.startActivity(
+                Intent(it.context,DetailActivity::class.java)
+                    .putExtra("EXTRA_DATA_DOA", listDoa[position])
+            )
         }
     }
 
