fd7d41f866f8cd5c1ece82f94dc0e84a5fa4aac8
haydarahmad <fattanhay0@gmail.com>
2022-07-10 00:15:47 +0700

    mainActivity (belum kelar)

--- app/src/main/java/com/haydar/taawun_app/MainActivity.kt
+++ app/src/main/java/com/haydar/taawun_app/MainActivity.kt
@@ -3,10 +3,37 @@ package com.haydar.taawun_app
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.haydar.taawun_app.databinding.ActivityMainBinding
+import com.haydar.taawun_app.databinding.FragmentDoaBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = binding.
+        setContentView(binding.root)
+
+        val navView : BottomNavigationView = binding.myNav
+
+        val navControler = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_doa,
+                R.id.nav_dzikir,
+                R.id.nav_home
+            )
+        )
+        setupActionBarWithNavController(navControler, appBarConfiguration)
+        navView.setupWithNavController(navControler)
     }
+
 }
